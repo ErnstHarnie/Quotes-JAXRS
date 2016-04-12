6c58897b3d632e73c9ceb0daa7b83b4785b8044e
ernstharnie <ernst_harnie@hotmail.com>
2016-04-12 16:54:28 +0200

    Method to fill DB + get quotes without parameter

--- src/edu/ap/quotes/Quotes.java
+++ src/edu/ap/quotes/Quotes.java
@@ -1,6 +1,11 @@
 package edu.ap.quotes;
 
 import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
 import javax.ws.rs.*;
 import javax.json.*;
 
@@ -17,35 +22,36 @@ public class Quotes {
 	public String getQuotesHTML() {
 		String htmlString = "<html><body>";
 		try {
-			JsonReader reader = Json.createReader(new StringReader(getRedisDB()));
-			JsonObject rootObj = reader.readObject();
-			JsonArray array = rootObj.getJsonArray("products");
+			FillRedisDatabase();
+
+			ArrayList<String> quotes = new ArrayList<String>();
+			ArrayList<String> authors = new ArrayList<String>(); 
+
+			quotes.add(redis.get("quote:*"));
+			quotes.add(redis.get("author:*"));
 			
-			
-			for(int i = 0 ; i < array.size(); i++) {
-				JsonObject obj = array.getJsonObject(i);
-				htmlString += "<b>Name : " + obj.getString("name") + "</b><br>";
-				htmlString += "ID : " + obj.getString("id") + "<br>";
-				htmlString += "Brand : " + obj.getString("brand") + "<br>";
-				htmlString += "Description : " + obj.getString("description") + "<br>";
-				htmlString += "Price : " + obj.getString("price") + "<br>";
-				htmlString += "<br><br>";
+			for(int i = 0 ; i < quotes.size(); i++) {
+				htmlString += "<b>Author : " + authors + "</b><br>";
+				htmlString += "Quote : " + quotes + "<br>";
 			}
 		}
 		catch(Exception ex) {
 			htmlString = "<html><body>" + ex.getMessage();
 		}
+		
+		return htmlString;
 	
 }
 	
-	@POST
-	@Consumes({"application/json"})
 	public String FillRedisDatabase() {
 		String returnCode = "";
 		try {
 			redis.set("quote:1", "I may be drunk, Miss, but in the morning I will be sober and you will still be ugly.");
 			redis.set("quote:1", "You have enemies? Good. That means you've stood up for something, sometime in your life.");
 			redis.set("quote:2", "No doubt exists that all women are crazy; it's only a question of degree.");
+			redis.set("author:1", "Winston Churchill");
+			redis.set("author:2", "Albert Einstein");
+			
 		} 
 		catch (Exception ex) {
 			returnCode = ex.getMessage();
@@ -54,19 +60,19 @@ public class Quotes {
 		return returnCode;
 	}
 	
-	@GET
-	@Produces({"application/json"})
-	public String getRedisDB() {
-		String jsonString = "";
+
+	public String getAuthorsDB() {
+		String string = "";
 		try {
-			redis.get("quote:*");
+			
+			
 			
 		} 
 		catch (Exception ex) {
-			jsonString = ex.getMessage();
+			string = ex.getMessage();
 		}
 		
-		return jsonString;
+		return string;
 	}
 	
 	
